f43eb26241792e2829f30ce4c467079bca70ecc1
EdwardTerran <edwardterran@gmail.com>
2019-04-15 08:26:29 -0500

    adds first and second stages

---
+++ GameAIFinal/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+
+    Vector3 pos;
+
+    public void Start()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        
+
+        if (Input.GetKey("a"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("w"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
